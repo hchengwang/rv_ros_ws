2ce4d58c128c2670dd68c6ac090c384d29768047
hchengwang <hchengwang@csail.mit.edu>
2016-05-03 22:06:20 +0800

    revert depthmap_ros_to_lcm to the state before adding ZED camera; some bugs there...

--- src/depthmap_ros_to_lcm/src/depthmap_ros_to_lcm.cpp
+++ src/depthmap_ros_to_lcm/src/depthmap_ros_to_lcm.cpp
@@ -19,35 +19,44 @@ cvBridgeLCM* cv_bridge_lcm;
 void callback(const sensor_msgs::ImageConstPtr& depthMap)
 {
     cv_bridge::CvImagePtr cv_ptr;
-    cv_ptr = cv_bridge::toCvCopy(depthMap, depthMap->encoding); //copy image
+    // cv_ptr = cv_bridge::toCvCopy(depthMap, depthMap->encoding); //copy image
+    cv_ptr = cv_bridge::toCvCopy(depthMap, sensor_msgs::image_encodings::TYPE_32FC1); //copy image
+    
     cv::Mat im_kinect_rgb = cv::Mat::zeros(480, 640, CV_8UC3);
     cv::Mat im_kinect_depth = cv::Mat::zeros(480, 640, CV_16UC1);
     
     cv::Mat im_depth = cv_ptr->image.clone();
-    int scale = 1;
-    int d_scale = 1;
-    if(depthMap->width == 640 && depthMap->height == 480) //ZED
-    {
-        ROS_INFO("ZED Camera 640x480");
-    }
-    else if( depthMap->width == 320 && depthMap->height == 240) //Structure
-    {
-        ROS_INFO("Structure Sensor 320x240");
-        scale = 2;
-        d_scale = 1000; //structure is in meters apparantly
-    }
-    else
-    {
-        ROS_INFO("UNDEFINED RESOLUTION DETECTED %dx%d",depthMap->width, depthMap->height);
-        scale = 0;
-    }
+
+//    // for ZED
+//    int scale = 1;
+//    int d_scale = 1;
+//    if(depthMap->width == 640 && depthMap->height == 480) //ZED
+//    {
+//        ROS_INFO("ZED Camera 640x480");
+//    }
+//    else if( depthMap->width == 320 && depthMap->height == 240) //Structure
+//    {
+//        ROS_INFO("Structure Sensor 320x240");
+//        scale = 2;
+//        d_scale = 1000; //structure is in meters apparantly
+//    }
+//    else
+//    {
+//        ROS_INFO("UNDEFINED RESOLUTION DETECTED %dx%d",depthMap->width, depthMap->height);
+//        scale = 0;
+//    }
+
     // [y, x]: [240, 320] -> [480, 640]
+    int scale = 2; // structure sensor
+
     for(int i = 0; i < im_depth.cols - 2; i++){
         for(int j = 0; j < im_depth.rows - 2; j++){
             int x = i * scale;
             int y = j * scale;
             // float m -> int mm
-            int16_t d = (int16_t)(im_depth.at<int16_t>(j, i)) * d_scale; 
+            //int16_t d = (int16_t)(im_depth.at<int16_t>(j, i)) * d_scale; 
+            int16_t d = (int16_t)(im_depth.at<float>(j, i)) * 1000;
+
             im_kinect_depth.at<int16_t>(y, x) =     d;
             if(scale == 2)
             {
